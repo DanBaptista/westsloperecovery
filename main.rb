652baff16b04cd91a6c28cd391c7cdfdd493ad79
Nati <nati.tessema@gmail.com>
2014-04-15 16:03:51 -0700

    Getting ruby hooked up with a local html document.

---
+++ main.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+# testing browser functionality
+wsr_page = "http://www.westsloperecovery.org"
+my_html_page = "index.html"
+system("open", my_html_page)
--- test.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-
-# testing browser functionality
-system("open", "http://www.westsloperecovery.org")
