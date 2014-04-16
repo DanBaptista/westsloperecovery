75af684c5d5d2f34a137d68a771babff2a26a188
Nati <nati.tessema@gmail.com>
2014-04-15 22:16:17 -0700

    Added support for windows.

--- main.rb
+++ main.rb
@@ -1,6 +1,17 @@
 #!/usr/bin/env ruby
 
+# finding OS
+is_a_mac = RUBY_PLATFORM.downcase.include? "darwin"
+is_a_pc = RUBY_PLATFORM.downcase.include? "mswin"
+is_a_linux = RUBY_PLATFORM.downcase.include? "linux"
+
 # testing browser functionality
 wsr_page = "http://www.westsloperecovery.org"
 my_html_page = "index.html"
-system("open", my_html_page)
+if is_a_mac
+    system("open", my_html_page)
+elsif is_a_pc
+    system("start #{my_html_page}")
+else
+    print 'unrecognized OS\n'
+end
