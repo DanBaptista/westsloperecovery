71f9d804a9fd7f3a2f94964cf6e4f9fb30f4df55
Nati <nati.tessema@gmail.com>
2014-04-15 22:47:26 -0700

    Trying to get the script to recognize Derek's pc.

--- main.rb
+++ main.rb
@@ -3,14 +3,16 @@
 # finding OS
 is_a_mac = RUBY_PLATFORM.downcase.include? "darwin"
 is_a_pc = RUBY_PLATFORM.downcase.include? "mswin"
+is_dereks_pc = RUBY_PLATFORM.downcase.include? "ming"
 is_a_linux = RUBY_PLATFORM.downcase.include? "linux"
 
 # testing browser functionality
 wsr_page = "http://www.westsloperecovery.org"
 my_html_page = "index.html"
+
 if is_a_mac
     system("open", my_html_page)
-elsif is_a_pc
+elsif is_a_pc or is_dereks_pc
     system("start #{my_html_page}")
 elsif is_a_linux
     system "xdg-open #{my_html_page}"
